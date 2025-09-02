0741f359f22c36454f31285e11f1bda4c118f6b9
Darnes1958 <abdel_ati_m@yahoo.com>
2025-09-02 18:02:16 +0200

    any

--- config/database.php
+++ config/database.php
@@ -497,12 +497,12 @@ return [
         'prefix_indexes' => true,
         'trust_server_certificate' => true,
       ],
-      'MotTest' => [
+      'Botnan' => [
         'driver' => 'sqlsrv',
         'url' => env('DATABASE_URL_OTHER'),
         'host' => env('DB_HOST_OTHER', 'localhost'),
         'port' => env('DB_PORT_OTHER', '1433'),
-        'database' => env('DB_DATABASE_MotTest', 'forge'),
+        'database' => env('DB_DATABASE_Botnan', 'forge'),
         'username' => env('DB_USERNAME_OTHER', 'forge'),
         'password' => env('DB_PASSWORD_OTHER', ''),
         'charset' => 'utf8',
