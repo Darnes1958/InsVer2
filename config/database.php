e9b7a7bbd8855d2fa8c9d4fa4e5191c4c9035528
Darnes1958 <abdel_ati_m@yahoo.com>
2025-10-25 19:56:35 +0200

    any

--- config/database.php
+++ config/database.php
@@ -503,7 +503,7 @@ return [
             'url' => env('DATABASE_URL_OTHER'),
             'host' => env('DB_HOST_OTHER', 'localhost'),
             'port' => env('DB_PORT_OTHER', '1433'),
-            'database' => env('DB_DATABASE_Ather', 'forge'),
+            'database' => env('DB_DATABASE_Atheer', 'forge'),
             'username' => env('DB_USERNAME_OTHER', 'forge'),
             'password' => env('DB_PASSWORD_OTHER', ''),
             'charset' => 'utf8',
